3506782e7f99799611de8f40c813a03debd32e1c
m1cro1ce <shams.s@foundationai.com>
2018-09-04 13:02:29 +0530

    ADD: logic to get readable pdf from tesseract engine

--- tesseract/app.py
+++ tesseract/app.py
@@ -1,22 +1,48 @@
 # Pytesseract Reference: https://pypi.org/project/pytesseract/
 # Tesseract Reference: https://github.com/tesseract-shadow/tesseract-ocr-re
 
-from flask import Flask, request
+from flask import Flask, request, send_file
 
+import os
 import pytesseract
 from PIL import Image
+import subprocess
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+@app.route("/image_to_readable_pdf", methods=["POST"])
+def get_readable_pdf():
+	file = request.files.getlist("file")
+	file = file[0]
+	filename = file.filename
+	file.save(filename)
+
+	if 'output_file.pdf' in os.listdir('.'):
+		os.remove('output_file.pdf')
+
+	subprocess.call(
+		"tesseract " + \
+		filename + \
+		" output_file --psm 6 --oem 3 pdf",
+		shell=True
+	)
+	os.remove(filename)
+
+	res = None
+	if 'output_file.pdf' in os.listdir('.'):
+		return send_file('output_file.pdf')
+
+	return 'None'
+
+
 @app.route("/image_to_string", methods=["POST"])
 def image_to_string():
-	print(request.files)
 	file = request.files.getlist("file")
 	image = Image.open(file[0])
-	result = pytesseract.image_to_string(image)
+	result = pytesseract.image_to_string(image, config="--psm 4")
 
 	return result
 
@@ -43,7 +69,6 @@ def image_to_osd():
 	result = pytesseract.image_to_osd(image)
 
 	return result
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888)
